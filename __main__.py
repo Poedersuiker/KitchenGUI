ea3eb0d453f62cf379b244fbeccc987f63d4bd07
Peter Hendriks <poederbuik@hotmail.com>
2017-06-18 15:10:43 +0200

    initial test

---
+++ __main__.py
@@ -0,0 +1,77 @@
+import gi
+gi.require_version("Gtk", "3.0")
+gi.require_version('WebKit', '3.0')
+from gi.repository import Gtk, Gdk, WebKit
+
+
+class SmartHomeWindow(Gtk.Window):
+    def __init__(self):
+        self.webpages = []
+
+        Gtk.Window.__init__(self, title="SmartHome Kitchen")
+        # self.fullscreen()
+        self.set_border_width(3)
+
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        self.chat_page = Gtk.Box()
+        self.chat_page.set_border_width(10)
+        self.chat_page.add(Gtk.Label("Google Assistant Chat"))
+        self.notebook.append_page(self.chat_page, Gtk.Label("GoogleA chat"))
+
+        self.home_page = Gtk.Box()
+        self.home_page.set_border_width(10)
+        self.home_page.add(Gtk.Label("Home Page"))
+        self.notebook.append_page(self.home_page, Gtk.Label("Smart Home Page"))
+
+        web_page = WebKit.WebView()
+        web_page.load_uri("https://www.google.com")
+        self.add_tab(web_page, 'Google')
+        self.webpages.append(web_page)
+
+        new_img = Gtk.Image()
+        new_img.set_from_stock(Gtk.STOCK_NEW, Gtk.IconSize.MENU)
+        self.notebook.
+
+
+    def add_tab(self, widget, label_str):
+        hbox = Gtk.HBox(False, 0)
+        label = Gtk.Label(label_str)
+        hbox.pack_start(label, True, True, 0)
+
+        close_img = Gtk.Image()
+        close_img.set_from_stock(Gtk.STOCK_CLOSE, Gtk.IconSize.MENU)
+
+        btn = Gtk.Button()
+        btn.set_relief(Gtk.ReliefStyle.NONE)
+        btn.set_focus_on_click(False)
+        btn.add(close_img)
+        hbox.pack_start(btn, False, False, 0)
+
+        btn.connect('clicked', self.on_closetab_button_clicked, widget)
+
+        hbox.show_all()
+        self.notebook.append_page(widget, hbox)
+
+    def on_closetab_button_clicked(self, sender, widget):
+        pagenum = self.notebook.page_num(widget)
+        self.notebook.remove_page(pagenum)
+
+    def on_newtab_button(self):
+        print("Pressed new")
+
+def keypress(window, event):
+    key = Gdk.keyval_name(event.keyval)
+    print(key)
+
+    if key == "F11":
+        window.is_fullscreen = not getattr(window, 'is_fullscreen', False)
+        action = window.fullscreen if window.is_fullscreen else window.unfullscreen
+        action()
+
+window = SmartHomeWindow()
+window.connect("delete-event", Gtk.main_quit)
+window.connect("key-press-event", keypress)
+window.show_all()
+Gtk.main()
